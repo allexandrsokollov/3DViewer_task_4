a1dc810563f5008ecc89dda966cf1f703f7dc173
NinelNina <ninkr@list.ru>
2023-01-06 18:50:52 +0300

    Add rasterizaton with z-buffer algorithm.

--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -1,6 +1,9 @@
 package com.cgvsu.render_engine;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.cgvsu.math.Matrix4;
 import com.cgvsu.math.Vector3f;
@@ -46,6 +49,9 @@ public class RenderEngine {
                 resultPoints.add(resultPoint);
             }
 
+            rasterize(graphicsContext, mesh, polygonInd, resultPoints);
+
+            graphicsContext.setStroke(Color.WHITE);
             for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 graphicsContext.strokeLine(
                         resultPoints.get(vertexInPolygonInd - 1).x,
@@ -60,6 +66,62 @@ public class RenderEngine {
                         resultPoints.get(nVerticesInPolygon - 1).y,
                         resultPoints.get(0).x,
                         resultPoints.get(0).y);
+        }
+    }
+
+    private static void rasterize(GraphicsContext graphicsContext, Model mesh, int polygonInd, List<Point2f> resultPoints){
+        BufferedImage img = new BufferedImage((int) graphicsContext.getCanvas().getWidth(), (int) graphicsContext.getCanvas().getHeight(), BufferedImage.TYPE_INT_ARGB);
+        float[] zBuffer = new float[img.getWidth() * img.getHeight()];
+
+        Arrays.fill(zBuffer, Float.NEGATIVE_INFINITY);
+
+        rasterizePolygon(graphicsContext,
+                img,
+                mesh.getVertices().get(mesh.getPolygons().get(polygonInd).getVertexIndices().get(0)),
+                mesh.getVertices().get(mesh.getPolygons().get(polygonInd).getVertexIndices().get(1)),
+                mesh.getVertices().get(mesh.getPolygons().get(polygonInd).getVertexIndices().get(2)),
+                resultPoints,
+                zBuffer);
+    }
+
+    private static void rasterizePolygon(
+            GraphicsContext graphicsContext,
+            BufferedImage img,
+            Vector3f v1,
+            Vector3f v2,
+            Vector3f v3,
+            List<Point2f> points,
+            float[] zBuffer){
+
+        float x1 = points.get(0).x;
+        float y1 = points.get(0).y;
+        float x2 = points.get(1).x;
+        float y2 = points.get(1).y;
+        float x3 = points.get(2).x;
+        float y3 = points.get(2).y;
+
+        int minX = (int) Math.max(0, Math.ceil(Math.min(x1, Math.min(x2, x3))));
+        int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(x1, Math.max(x2, x3))));
+        int minY = (int) Math.max(0, Math.ceil(Math.min(y1, Math.min(y2, y3))));
+        int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(y1, Math.max(y2, y3))));
+
+        float triangleArea = (y1 - y3) * (x2 - x3) + (y2 - y3) * (x3 - x1);
+        for (int y = minY; y <= maxY; y++) {
+            for (int x = minX; x <= maxX; x++) {
+                float b1 = ((y - y3) * (x2 - x3) + (y2 - y3) * (x3 - x)) / triangleArea;
+                float b2 = ((y - y1) * (x3 - x1) + (y3 - y1) * (x1 - x)) / triangleArea;
+                float b3 = ((y - y2) * (x1 - x2) + (y1 - y2) * (x2 - x)) / triangleArea;
+
+                if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                    float depth = b1 * v1.getZ() + b2 * v2.getZ() + b3 * v3.getZ();
+                    int zIndex = y * img.getWidth() + x;
+                    if (zBuffer[zIndex] < depth) {
+                        graphicsContext.setStroke(Color.RED);
+                        graphicsContext.strokeLine(x, y, x, y);
+                        zBuffer[zIndex] = depth;
+                    }
+                }
+            }
         }
     }
 }
